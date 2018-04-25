ba4a478c06174ed3938f9042fab52157734ace0e
gabrielecimolino <gabrielecimolino@gmail.com>
2018-04-25 13:05:57 -0400

    We're live

---
+++ scripts/player.js
@@ -0,0 +1,26 @@
+console.clear();
+
+class musicPlayer {
+	constructor() {
+		this.play = this.play.bind(this);
+		this.playBtn = document.getElementById('playButton');
+		this.playBtn.addEventListener('click', this.play);
+		this.controlPanel = document.getElementById('control-panel');
+
+		this.music = document.getElementById('music');
+		this.chefMenu = document.getElementById('chefMenu');
+	}
+
+	play() {
+		let controlPanelObj = this.controlPanel
+
+		if(controlPanelObj.classList.contains("active")) this.music.pause();
+		else this.music.play();
+
+		Array.from(controlPanelObj.classList).find(function(element){
+					return element !== "active" ? controlPanelObj.classList.add('active') : controlPanelObj.classList.remove('active');
+			});
+	}
+}
+
+const newMusicplayer = new musicPlayer();
